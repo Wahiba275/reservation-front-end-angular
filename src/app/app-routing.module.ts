0caf64ef8d0427cae72d4286cc501cac3db45ee0
Wahiba BOUZYAN <ouahibabouzyan@gmail.com>
2023-12-28 13:48:16 +0100

    c2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {RessourcesComponent} from "./ressources/ressources.component";
+import {PersonnesComponent} from "./personnes/personnes.component";
+import {ReservationsComponent} from "./reservations/reservations.component";
+import {AuthGuard} from "./guards/auth.guard";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'ressources', component: RessourcesComponent },
+  { path: 'personnes', component: PersonnesComponent , canActivate:[AuthGuard], data : { roles:['USER'] }},
+  { path: 'reservations', component: ReservationsComponent },
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule{ }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,77 @@
-import { NgModule } from '@angular/core';
+import {APP_INITIALIZER, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
+import { AppComponent } from './app.component';
+import { PersonnesComponent } from './personnes/personnes.component';
+import { ReservationsComponent } from './reservations/reservations.component';
+import { RessourcesComponent } from './ressources/ressources.component';
+import {HttpClientModule} from "@angular/common/http";
+import {AppRoutingModule} from "./app-routing.module";
+import {RouterModule, Routes} from "@angular/router";
+import {KeycloakAngularModule, KeycloakService} from "keycloak-angular";
+import {AuthGuard} from "./guards/auth.guard";
+const routes: Routes = [
+  { path: 'ressources', component: RessourcesComponent },
+  { path: 'personnes', component: PersonnesComponent , canActivate:[AuthGuard], data : { roles:['USER'] }},
+  { path: 'reservations', component: ReservationsComponent },
+];
+
+
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'http://localhost:8080',
+        realm: 'sdia-realm',
+        clientId: 'reservation-web-client'
+      },
+      initOptions: {
+        onLoad: 'check-sso',
+        silentCheckSsoRedirectUri:
+          window.location.origin + '/assets/silent-check-sso.html'
+      }
+    });
+}
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonnesComponent,
+    ReservationsComponent,
+    RessourcesComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+    KeycloakAngularModule
+
   ],
-  providers: [],
+  exports:[RouterModule],
+  providers: [{provide : APP_INITIALIZER, deps : [KeycloakService],useFactory : initializeKeycloak, multi : true}],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule{ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
